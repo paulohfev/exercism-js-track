8b734eb8d1d2ac3555497b213fc223a79486ad6e
paulohfev <phfernandesev@gmail.com>
2021-03-14 23:43:58 -0300

    Add triangle exercise solution

---
+++ triangle/triangle.js
@@ -0,0 +1,30 @@
+export class Triangle {
+  constructor(...sides) {
+    this.sides = sides;
+    this.uniqueSides = Array.from(new Set(sides));
+  }
+
+  isTriangle(sides) {
+    function checkTriangleInequality() {
+      const sortSides = sides.sort((a, b) => a - b);
+      return sortSides[0] + sortSides[1] >= sortSides[2];
+    }
+
+    return sides.every(side => side != 0) && checkTriangleInequality();
+  }
+
+  get isEquilateral() {
+    const uniqueSides = this.uniqueSides;
+    return this.isTriangle(this.sides) && uniqueSides.length === 1;
+  }
+
+  get isIsosceles() {
+    const uniqueSides = this.uniqueSides;
+    return this.isTriangle(this.sides) && (uniqueSides.length === 2 || uniqueSides.length === 1);
+  }
+
+  get isScalene() {
+    const uniqueSides = this.uniqueSides;
+    return this.isTriangle(this.sides) && uniqueSides.length === 3;
+  }
+}
